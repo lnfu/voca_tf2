76b1d2d3373525e95757c4e740a93d1d19b7e37a
Enfu Liao <efliao.cs.nycu.edu.tw>
2024-10-04 15:03:58 +0800

    feat: 輸出影片 (影片是全灰, 之後再解決)

--- README.md
+++ README.md
@@ -3,13 +3,18 @@
 ```sh
 docker run --gpus all -it -v .:/workspace  --rm nvcr.io/nvidia/tensorflow:23.07-tf2-py3 bash
 # docker run --gpus all -it -v .:/workspace  --rm nvcr.io/nvidia/tensorflow:24.09-tf2-py3 bash
-pip install pyyaml resampy python_speech_features scipy
+pip install pyyaml resampy python_speech_features scipy trimesh pyrender
 export TF_CPP_MIN_LOG_LEVEL=1
 
+apt install -y ffmpeg 
+apt install -y libosmesa6
+
 cd mesh
-apt update
-apt install libboost-dev python3-opengl
+apt update -y
+apt install -y libboost-dev 
+# apt install python3-opengl
 BOOST_INCLUDE_DIRS=/usr/include/boost make all
+cd ..
 ```
 
 ## 規範
@@ -17,6 +22,22 @@ BOOST_INCLUDE_DIRS=/usr/include/boost make all
 - mesh = vertex + face
 - pcd (point cloud) = shape(5023, 3)
 
+mesh to image 使用 pyrender
+
+影片加上音訊使用 ffmpeg
+
+<!-- pyrender 需要安裝 mesa (opengl 相關)
+```sh
+apt update
+wget https://github.com/mmatl/travis_debs/raw/master/xenial/mesa_18.3.3-0.deb
+dpkg -i ./mesa_18.3.3-0.deb || true
+apt install -f
+apt --fix-broken install
+``` -->
+
+忽略 pyrender 對 pyopengl 版本的要求: https://blog.csdn.net/guntangsanjing/article/details/127651381
+
+pyopengl 必須安裝 3.1.4
 
 ## 🎯 TODO
 
--- run.py
+++ run.py
@@ -3,7 +3,10 @@ import tensorflow as tf
 
 import os
 import sys
+import cv2
 import logging
+import trimesh
+import pyrender
 
 from psbody.mesh import Mesh
 from scipy.io import wavfile
@@ -12,7 +15,6 @@ from utils.config import load_config
 from utils.data_handlers.audio_handler import AudioHandler
 from utils.batcher import Batcher
 from utils.model import custom_loss
-
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -65,12 +67,81 @@ def main():
         ]
     )
 
-    print(pred_pcds.shape)
+    assert num_frames == pred_pcds.shape[0]
 
-    # render
+    # pred_pcds.shape = (?, 5023, 3)
+    centers = np.mean(pred_pcds, axis=1)  # (?, 3)
+    center = np.mean(centers, axis=0)  # (3, )
+
+    # save
+    # with open("output/sample.mp4", "w") as f:
+    video_writer = cv2.VideoWriter("outputs/sample.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 60, (800, 800), True)
     for i, pcd in enumerate(pred_pcds):
+        print(i)
         mesh = Mesh(pcd, template.f)
         mesh.write_obj(os.path.join("outputs/meshes/", "%05d.obj" % i))
+
+        image = render_mesh_to_image(mesh=mesh, center=center)
+        video_writer.write(image)
+    video_writer.release()
+
+
+def render_mesh_to_image(mesh, center, rotation=np.zeros(3)):
+
+    camera_params = {
+        "optical_center": [400.0, 400.0],
+        "focal_length": [4754.97941935 / 2, 4754.97941935 / 2],  # TODO 數值???
+    }
+
+    frustum_params = {"near": 0.01, "far": 3.0, "height": 800, "width": 800}
+
+    # 建立場景
+    scene = pyrender.Scene(ambient_light=[0.2, 0.2, 0.2], bg_color=[255, 255, 255])
+
+    # 加入 camera
+    camera = pyrender.IntrinsicsCamera(
+        fx=camera_params["focal_length"][0],
+        fy=camera_params["focal_length"][1],
+        cx=camera_params["optical_center"][0],
+        cy=camera_params["optical_center"][1],
+        znear=frustum_params["near"],
+        zfar=frustum_params["far"],
+    )
+    scene.add(camera, pose=np.eye(4))
+
+    # 加入光源
+    light = pyrender.PointLight(color=np.array([1.0, 1.0, 1.0]), intensity=1.5)
+    scene.add(light, pose=np.eye(4))
+
+    mesh_copy = Mesh(mesh.v, mesh.f)
+
+    # 圍繞 center 做旋轉 rotation
+    mesh_copy.v[:] = cv2.Rodrigues(rotation)[0].dot((mesh_copy.v - center).T).T + center
+
+    # trimesh 上色 (沒有色彩)
+    tri_mesh = trimesh.Trimesh(
+        vertices=mesh_copy.v, faces=mesh_copy.f, vertex_colors=None
+    )
+
+    # pyrender mesh
+    render_mesh = pyrender.Mesh.from_trimesh(tri_mesh, smooth=True)
+    scene.add(render_mesh, pose=np.eye(4))
+
+    try:
+        renderer = pyrender.OffscreenRenderer(
+            viewport_width=frustum_params["width"],
+            viewport_height=frustum_params["height"],
+        )
+        color, _ = renderer.render(scene, flags=pyrender.RenderFlags.SKIP_CULL_FACES)
+    except Exception as e:
+        print(e)
+        print("pyrender: Failed rendering frame")
+        color = np.zeros(
+            (frustum_params["height"], frustum_params["width"], 3), dtype="uint8"
+        )
+
+    return color[..., ::-1]
+
 
 if __name__ == "__main__":
     main()
